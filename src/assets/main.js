1287dfcfda19f740118e1146c4d3b996e930bcb2
MagerTom <sunny@olly.co>
2017-05-04 13:42:49 +1000

    updated with jquery coding
    created the Courses; based on the solution for the ajax jquery project.
    
    added the Badges section to complete the picture (and read in the
    additional array from teh JSON output).

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,72 @@ $(function() {
 
   // your code will go here
 
+  $.ajax({
+  url: 'https://www.codeschool.com/users/3570413.json',
+  dataType: 'jsonp',
+  success: function(data) {
+    // handle response
+    populateWithCourses(data.courses.completed);
+    populateWithBadges(data.badges);
+
+
+  }
+  });
+
+  function populateWithCourses(courses) {
+
+    var $courses = $('#courses');
+
+    courses.forEach(function(course) {
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($courses);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
+
+  function populateWithBadges(badges) {
+
+    var $badges = $('#badges');
+
+    badges.forEach(function(badge) {
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: badge.name
+      }).appendTo($div);
+
+      $('<img />', {
+        src: badge.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: badge.course_url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
+
 });
